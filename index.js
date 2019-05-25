615bfd5bc4b36d693ff02fff8de34c6a899f12cc
Cameron Steele <cam.steeleis@gmail.com>
2019-05-25 09:09:11 -0500

    Added The ability to recognize devices and choose which to interact with

--- index.js
+++ index.js
@@ -16,26 +16,107 @@ const supportedGames = {
     //"com.beatgames.beatsaber.demo": { title: "BeatSaber Demo", savefile: "files/PlayerData.dat", appid: "com.beatgames.beatsaber.demo" }
 }
 
-function scanForGames() {
-    return client.listDevices()
-        .then(function (devices) {
-            return Promise.map(devices, function (device) {
-                return client.readdir(device.id, path)
-                    .then(function (files) {
-                        var recognizedGames = [];
-                        files.forEach(function (file) {
-                            if (file.name in supportedGames) {
-                                //console.log(`Found: ${supportedGames[file.name]}`)
-                                recognizedGames.push({ title: supportedGames[file.name].title, path: `${path}${file.name}/${supportedGames[file.name].savefile}`, appid: file.name})
-                            }
-                        })
-                        return recognizedGames;
-                    })
+function scanForDevices() {
+    return client.listDevices().then(devices => {
+        console.log(devices)
+        return Promise.map(devices, device => {
+            return client.getProperties(device.id).then(props => {
+                return { id: device.id, brand: props['ro.product.manufacturer'], model: props['ro.product.model'] };
             })
         })
-        .catch(function (err) {
-            console.log(err)
+    }).catch(err => {
+        console.log("I am an error on line 28: ", err)
+    })
+}
+
+
+function promptForDevice() {
+    return scanForDevices().then(function (devices) {
+        let deviceOptions = devices.filter(device => {
+            return device.brand !== "oculus"
+        }).map(device => {
+            return { name: `${device.brand} ${device.model}`, value: device.id }
         })
+        deviceOptions.push({ name: "Search Again", value: "searchAgain" })
+        deviceOptions.push({ name: "Quit", value: "quit" })
+        inquirer.prompt([{
+            type: 'list',
+            name: 'chosenDevice',
+            message: 'Please Select Your Device',
+            choices: deviceOptions
+        }]).then(answers => {
+            switch (answers.chosenDevice) {
+                case "searchAgain":
+                    promptForDevice()
+                    break
+                case "quit":
+                    process.exit()
+                    break
+                default:
+                    return answers.chosenDevice
+            }
+        }).then(deviceid => {
+            scanForGames(deviceid).then(gamesList => {
+                let foundGames = gamesList;
+                let gameChoices = foundGames.map(function (item) {
+                    return { name: item.title, value: item }
+                })
+                //console.log(gameChoices)
+                inquirer.prompt([
+                    {
+                        type: 'list',
+                        name: "selectedGame",
+                        message: "Which game would you like to work on?",
+                        choices: gameChoices
+                    },
+                    {
+                        type: 'list',
+                        name: "action",
+                        message: "What would you like to do?",
+                        choices: [
+                            { name: "Backup", value: { action: 0, deviceid } },
+                            { name: "Restore", value: { action: 1, deviceid } },
+                            { name: "Reset", value: { action: 2, deviceid } },
+                        ]
+                    }
+                ]).then(answers => {
+                    switch (answers.action.action) {
+                        case 0:
+                            backupGameFile(answers.action.deviceid, answers.selectedGame)
+                            break
+                        case 1:
+                            restoreGameFile(answers.action.deviceid, answers.selectedGame)
+                            break
+                        case 2:
+                            resetSaveFile(answers.action.deviceid, answers.selectedGame)
+                            break
+                    }
+                }).catch(err => {
+                    console.log("I am an error on line 122: ", err)
+                })
+            })
+        }).catch(err => {
+
+        })
+    })
+}
+
+function scanForGames(deviceid) {
+    console.log("Have I happened yet")
+    if (deviceid === undefined) {
+        return
+    }
+    return client.readdir(deviceid, path)
+        .then(function (files) {
+            var recognizedGames = [];
+            files.forEach(function (file) {
+                if (file.name in supportedGames) {
+                    //console.log(`Found: ${supportedGames[file.name]}`)
+                    recognizedGames.push({ title: supportedGames[file.name].title, path: `${path}${file.name}/${supportedGames[file.name].savefile}`, appid: file.name })
+                }
+            })
+            return recognizedGames;
+        }).catch()
 
 }
 
@@ -45,49 +126,11 @@ function init() {
     }
     db.defaults({ saves: [], count: 0 })
         .write()
-    scanForGames().then(function (value) {
-        let foundGames = value[0];
-        //console.log(foundGames)
-        let gameChoices = foundGames.map(function (item) {
-            return { name: item.title, value: item }
-        })
-        //console.log(gameChoices)
-        inquirer.prompt([
-            {
-                type: 'list',
-                name: "selectedGame",
-                message: "Which game would you like to work on?",
-                choices: gameChoices
-            },
-            {
-                type: 'list',
-                name: "action",
-                message: "What would you like to do?",
-                choices: [
-                    { name: "Backup", value: 0 },
-                    { name: "Restore", value: 1 },
-                    { name: "Reset", value: 2 },
-                ]
-            }
-        ]).then(answers => {
-            switch (answers.action) {
-                case 0:
-                    backupGameFile(answers.selectedGame)
-                    break
-                case 1:
-                    restoreGameFile(answers.selectedGame)
-                    break
-                case 2:
-                    resetSaveFile(answers.selectedGame)
-                    break
-            }
-        }).catch(err => {
-            console.log(err)
-        })
-    })
+    promptForDevice()
+
 }
 
-function backupGameFile(gameinfo) {
+function backupGameFile(deviceid, gameinfo) {
     inquirer.prompt([
         {
             type: "input",
@@ -97,43 +140,36 @@ function backupGameFile(gameinfo) {
     ]).then(answers => {
         const id = shortid.generate()
         const savePath = `./saves/${gameinfo.title}-${id}-${answers.saveTitle}.questsaver`;
-        client.listDevices()
-            .then(function (devices) {
-                return Promise.map(devices, function (device) {
-                    return client.pull(device.id, gameinfo.path)
-                        .then(function (transfer) {
-                            return new Promise(function (resolve, reject) {
-                                var fn = savePath
-                                transfer.on('progress', function (stats) {
-                                    console.log('[%s] Pulled %d bytes so far',
-                                        device.id,
-                                        stats.bytesTransferred)
-                                })
-                                transfer.on('end', function () {
-                                    console.log('[%s] Pull complete', device.id)
-                                    resolve(device.id)
-                                })
-                                transfer.on('error', reject)
-                                transfer.pipe(fs.createWriteStream(fn))
-                            })
-                        })
+
+        return client.pull(deviceid, gameinfo.path)
+            .then(function (transfer) {
+                return new Promise(function (resolve, reject) {
+                    var fn = savePath
+                    transfer.on('progress', function (stats) {
+                        console.log('[%s] Pulled %d bytes so far',
+                            deviceid,
+                            stats.bytesTransferred)
+                    })
+                    transfer.on('end', function () {
+                        console.log('[%s] Pull complete', deviceid)
+                        resolve(deviceid)
+                    })
+                    transfer.on('error', reject)
+                    transfer.pipe(fs.createWriteStream(fn))
                 })
+            }).then(function () {
+                db.get('saves')
+                    .push({ id, name: answers.saveTitle, path: savePath, app: gameinfo.title, appid: gameinfo.appid })
+                    .write()
             })
-            .then(function () {
-                console.log('Done pulling file from all connected devices')
-            })
-            .catch(function (err) {
-                console.error('Something went wrong:', err.stack)
-            })
-        
-        db.get('saves')
-            .push({ id, name: answers.saveTitle, path: savePath, app: gameinfo.title, appid: gameinfo.appid })
-            .write()
+
+
+
     })
 
 }
 
-function restoreGameFile(gameinfo) {
+function restoreGameFile(deviceid, gameinfo) {
     //console.log(gameinfo) // title, path
     let saves = db.get('saves').value().filter(item => item.app == gameinfo.title)
     let options = saves.map(item => {
@@ -149,49 +185,36 @@ function restoreGameFile(gameinfo) {
     ]).then(answers => {
         let save = db.get('saves').find({ id: answers.saveid }).value()
         //console.log(save)
-        client.listDevices()
-            .then(function (devices) {
-                return Promise.map(devices, function (device) {
-                    return client.push(device.id, save.path, `${path}${save.appid}/${supportedGames[save.appid].savefile}`)
-                        .then(function (transfer) {
-                            return new Promise(function (resolve, reject) {
-                                transfer.on('progress', function (stats) {
-                                    console.log('[%s] Pushed %d bytes so far',
-                                        device.id,
-                                        stats.bytesTransferred)
-                                })
-                                transfer.on('end', function () {
-                                    console.log('[%s] Push complete', device.id)
-                                    resolve()
-                                })
-                                transfer.on('error', reject)
-                            })
-                        })
+
+        return client.push(deviceid, save.path, `${path}${save.appid}/${supportedGames[save.appid].savefile}`)
+            .then(function (transfer) {
+                return new Promise(function (resolve, reject) {
+                    transfer.on('progress', function (stats) {
+                        console.log('[%s] Pushed %d bytes so far',
+                            deviceid,
+                            stats.bytesTransferred)
+                    })
+                    transfer.on('end', function () {
+                        console.log('[%s] Push complete', deviceid)
+                        resolve()
+                    })
+                    transfer.on('error', reject)
                 })
             })
-            .then(function () {
-                console.log('Done pushing save file to all connected devices')
-            })
-            .catch(function (err) {
-                console.error('Something went wrong:', err.stack)
-            })
+
     })
 
 }
 
-function resetSaveFile(gameinfo){
-    client.listDevices()
-            .then(function (devices) {
-                return Promise.map(devices, function (device) {
-                    return client.clear(device.id, gameinfo.appid)
-                })
-            })
-            .then(function () {
-                console.log('Done Reseting Gamefile')
-            })
-            .catch(function (err) {
-                console.error('Something went wrong:', err.stack)
-            })
+function resetSaveFile(deviceid, gameinfo) {
+
+        return client.clear(deviceid, gameinfo.appid)
+        .then(function () {
+            console.log('Done Reseting Gamefile')
+        })
+        .catch(function (err) {
+            console.error('Something went wrong:', err.stack)
+        })
 
 }
 
